7196bd6ee2bf32e006fd5fc37d48192f58825936
Burhan Yusuf Arat <burhanyarat@gmail.com>
2023-12-21 00:07:26 +0300

    Update start_inference.py

--- start_inference.py
+++ start_inference.py
@@ -5,6 +5,9 @@ from inference_pipelines.inference_pipeline_maker import make_inference_pipeline
 from glob import glob
 import argparse
 from predict_utils import ScanSegmentation
+import math
+from trimesh import transformations
+import trimesh
 
 parser = argparse.ArgumentParser(description='Inference models')
 #parser.add_argument('--input_dir_path', default="G:/tooth_seg/main/all_datas/chl/3D_scans_per_patient_obj_files", type=str, help = "input directory path that contain obj files.")
@@ -13,8 +16,8 @@ parser.add_argument('--save_path', type=str, default="test_results", help = "res
 parser.add_argument('--model_name', type=str, default="tgnet", help = "model name. list: tsegnet | tgnet | pointnet | pointnetpp | dgcnn | pointtransformer")
 parser.add_argument('--checkpoint_path', default="ckpts/tgnet_fps" ,type=str,help = "checkpoint path.")
 parser.add_argument('--checkpoint_path_bdl', default="ckpts/tgnet_bdl" ,type=str,help = "checkpoint path(for tgnet_bdl).")
-parser.add_argument('--save_path_pre', default='/content/data' ,type=str,help = "save path for preprocessed obj's and basename.txt")
-parser.add_argument('--orig_input', default='/content/data' ,type=str,help = "STL inputs")
+parser.add_argument('--save_path_pre', required=True ,type=str,help = "save path for preprocessed obj's and basename.txt")
+parser.add_argument('--orig_input', required=True,type=str,help = "STL inputs")
 args = parser.parse_args()
 
 
@@ -30,26 +33,28 @@ def preprocess(orig_input,save_path_pre,split_txt_path):
 
         if not os.path.exists(os.path.join(save_path_pre,dirs)):
             os.makedirs(os.path.join(save_path_pre,dirs))
+        
+        if not os.path.exists(os.path.join(save_path_pre,dirs)):
+          sub_dirs = os.listdir(os.path.join(orig_input,dirs))
+          for sub_dir in sub_dirs:
+              if (dirs + '_UpperJaw.stl') == sub_dir:
+                  mesh = trimesh.load(os.path.join(orig_input,os.path.join(dirs,sub_dir)))
 
-        sub_dirs = os.listdir(os.path.join(path,dirs))
+                  rot_matrix_y = trimesh.transformations.rotation_matrix(angle*18, direction_y, center)
 
-        for sub_dir in sub_dirs:
-            if (dirs + '_UpperJaw.stl') == sub_dir:
-                mesh = trimesh.load(os.path.join(path,os.path.join(dirs,sub_dir)))
+                  mesh.apply_transform(rot_matrix_y)
 
-                rot_matrix_y = trimesh.transformations.rotation_matrix(angle*18, direction_y, center)
-
-                mesh.apply_transform(rot_matrix_y)
-
-                name = sub_dir.split('.')[0]
-                mesh.export(os.path.join(save_path_pre,os.path.join(dirs,name)+'.obj'))
+                  name = sub_dir.split('.')[0]
+                  mesh.export(os.path.join(save_path_pre,os.path.join(dirs,name)+'.obj'))
 
 
-            elif((dirs + '_LowerJaw.stl') == sub_dir):
+              elif((dirs + '_LowerJaw.stl') == sub_dir):
 
-                mesh = trimesh.load(os.path.join(path,os.path.join(dirs,sub_dir)))
-                name = sub_dir.split('.')[0]
-                mesh.export(os.path.join(save_path_pre,os.path.join(dirs,name)+'.obj'))
+                  mesh = trimesh.load(os.path.join(orig_input,os.path.join(dirs,sub_dir)))
+                  name = sub_dir.split('.')[0]
+                  mesh.export(os.path.join(save_path_pre,os.path.join(dirs,name)+'.obj'))
+        else:
+          print(dirs + '-- Exists')
 
 
     text_file = open(os.path.join(split_txt_path, 'basename.txt'), "a")
@@ -65,7 +70,7 @@ preprocess(args.orig_input,args.save_path_pre,args.split_txt_path)
 
 split_base_name_ls = []
 if args.split_txt_path != "":
-    f = open(args.split_txt_path, 'r')
+    f = open(os.path.join(args.split_txt_path,'basename.txt'), 'r')
     while True:
         line = f.readline()
         if not line: break
