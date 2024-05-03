99f60806934450478c5168d43935912b7de240c9
Burhan Yusuf Arat <burhan.arat@yeditepe.edu.tr>
2024-05-03 17:26:17 +0300

    Create environment_unittest.py

---
+++ unit_tests/environment_unittest.py
@@ -0,0 +1,20 @@
+import unittest
+
+
+class EnvironmentImportTest(unittest.TestCase):
+    
+    def test_case(self):
+        from inference_pipelines.inference_pipeline_maker import make_inference_pipeline
+        import pandas as pd
+        import json
+        import trimesh
+        import numpy as np
+        import math
+        import os
+        import open3d as o3d
+        import argparse
+        from tqdm import tqdm
+        
+        
+if __name__ == '__main__':
+    unittest.main()
